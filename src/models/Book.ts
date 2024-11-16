a5a3c505be3648e3b3605e8e4998d03e913e39d3
Eliseu Rodrigues <eliseu.rguimaraes@gmail.com>
2024-11-16 11:58:57 -0300

    :tada: Commit inicial

---
+++ src/models/Book.ts
@@ -0,0 +1,14 @@
+export class Book {
+    
+    codigo: number;
+    titulo: string;
+    autor: string;
+    disponivel: boolean;
+
+    constructor(codigo: number, titulo: string, autor: string, disponivel: boolean) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = disponivel;
+    }
+}
---
+++ src/models/Library.ts
@@ -0,0 +1,62 @@
+import { Book } from './Book';
+
+export class Library {
+    private acervo: Array<Book>;
+
+    constructor() {
+        this.acervo = new Array<Book>();
+    }
+
+    // Método para adicionar um livro ao acervo
+    public addBook(book: Book): boolean {
+        const exists = this.acervo.some(b => b.codigo === book.codigo);
+        if (!exists) {
+            this.acervo.push(book);
+            console.log(`[SRV-LIBRARY ✅] Livro adicionado: ${book.codigo} - ${book.titulo} (${book.autor})`);
+            //console.log('Acervo atual:', this.acervo);
+            return true;
+        } else {
+            console.log(`[SRV-LIBRARY 🔴] Livro com código ${book.codigo} já existe no acervo.`);
+            return false;
+        }
+    }
+
+    // Método para registrar um empréstimo de um livro
+    public registerLoan(codigo: number): string {
+
+        console.log(`[SRV-LIBRARY 🟡] Tentando registrar empréstimo para o livro com código: ${codigo}`);
+
+        let book = this.acervo.find(book => book.codigo === codigo);
+        if (!book) {
+            console.log(`[SRV-LIBRARY 🔴] Livro com código ${codigo} não encontrado.`);
+            return 'not_found';
+        }
+        if (!book.disponivel) {
+            console.log(`[SRV-LIBRARY 🔴] Livro não disponível para empréstimo.`);
+            return 'not_available';
+        }
+        book.disponivel = false;
+        console.log(`[SRV-LIBRARY ✅] Empréstimo registrado para o livro: ${book.titulo}`);
+        return 'success';
+    }
+
+    // Método para registrar a devolução de um livro
+    public registerReturn(codigo: number): void {
+        let book = this.acervo.find(book => book.codigo === codigo);
+        if (book) {
+            book.disponivel = true;
+            console.log(`[SRV-LIBRARY ✅] Devolução registrada para o livro: ${book.titulo}`);
+        }
+    }
+
+    // Método para retornar todos os livros disponíveis
+    public listAvailableBooks(): Array<Book> {
+        return this.acervo.filter(book => book.disponivel);
+    }
+
+    // Método para retornar todos os livros do acervo
+    public getAllBooks(): Array<Book> {
+        return this.acervo;
+    }
+    
+}
